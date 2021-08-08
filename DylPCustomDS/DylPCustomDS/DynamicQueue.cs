d4e7f696180dcb14dda284db8cdc4b613653e82b
Dylan Parrish <30538349+dparrish55@users.noreply.github.com>
2021-08-08 12:57:28 -0700

    DynamicQueue with tests
    Added a Queue for dynamics and appropriate tests for all methods.

---
+++ DylPCustomDS/DylPCustomDS/DynamicQueue.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DylPCustomDS
+{
+    public class DynamicQueue
+    {
+        public List<dynamic> elements = new List<dynamic>();
+        public int size = 0;
+
+        public DynamicQueue()
+        {
+            ;
+        }
+
+        public DynamicQueue(dynamic[] initializerArray)
+        {
+            for( int i = 0; i < initializerArray.GetLength(0); i++ )
+            {
+                this.Enqueue(initializerArray[i]);
+            }
+        }
+
+        public dynamic Dequeue()
+        {
+            dynamic topValue = this.elements[0];
+            this.elements.RemoveAt(0);
+            this.size -= 1;
+
+            return topValue;
+        }
+
+        public void Enqueue(dynamic valueToPush)
+        {
+            elements.Add(valueToPush);
+            this.size += 1;
+        }
+
+        public dynamic Next()
+        {
+            return this.elements[0];
+        }
+    }
+}
---
+++ DylPCustomDS/DylPCustomDSTests/DynamicQueueTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DynamicQueue = DylPCustomDS.DynamicQueue;
+using System;
+using System.Collections.Generic;
+
+namespace DylPCustomDSTests
+{
+    [TestClass]
+    public class DynamicQueueTests
+    {
+        [TestMethod]
+        public void newQueueWithArrayParameterPopsReverseElements()
+        {
+            dynamic[] initializerArray = new dynamic[] { 1, "2", 3, 4, 5 };
+            DynamicQueue newQueue = new DynamicQueue(initializerArray);
+
+            Assert.AreEqual(5, newQueue.size);
+
+            Assert.AreEqual(1, newQueue.Dequeue());
+            Assert.AreEqual(4, newQueue.size);
+
+            Assert.AreEqual("2", newQueue.Dequeue());
+            Assert.AreEqual(3, newQueue.size);
+
+            Assert.AreEqual(3, newQueue.Dequeue());
+            Assert.AreEqual(2, newQueue.size);
+
+            Assert.AreEqual(4, newQueue.Dequeue());
+            Assert.AreEqual(1, newQueue.size);
+
+            Assert.AreEqual(5, newQueue.Dequeue());
+            Assert.AreEqual(0, newQueue.size);
+        }
+
+        [TestMethod]
+        public void newQueueWithNoParametersIsEmpty()
+        {
+            DynamicQueue newQueue = new DynamicQueue();
+            List<dynamic> emptyDynamicList = new List<dynamic>();
+
+            Assert.AreEqual(0, newQueue.size);
+            CollectionAssert.AreEqual(emptyDynamicList, newQueue.elements);
+        }
+
+        [TestMethod]
+        public void TopReturnsTopElement()
+        {
+            dynamic[] initializerArray = new dynamic[] { 1, "2", 3, 4, 5 };
+            DynamicQueue newQueue = new DynamicQueue(initializerArray);
+
+            Assert.AreEqual(1, newQueue.Next());
+            Assert.AreEqual(5, newQueue.size);
+        }
+    }
+}
