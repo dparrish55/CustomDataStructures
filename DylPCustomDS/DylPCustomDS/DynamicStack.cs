9d01017e18eba7333a0d722e31c5bd61fa429399
Dylan Parrish <30538349+dparrish55@users.noreply.github.com>
2021-08-07 22:03:39 -0700

    Added DynamicStack and Tests
    Created a custom stack for dynamic types and tests thereto

---
+++ DylPCustomDS/DylPCustomDS/DynamicStack.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DylPCustomDS
+{
+    public class DynamicStack
+    {
+        public dynamic[] elements;
+        public int size;
+        private int ubound;
+
+        public DynamicStack()
+        {
+            this.elements = new dynamic[0];
+            this.size = 0;
+            this.ubound = 0;
+        }
+
+        public DynamicStack(dynamic[] initializerArray)
+        {
+            for( int i = 0; i < initializerArray.GetLength(0); i++ )
+            {
+                this.Push(initializerArray[i]);
+            }
+        }
+
+        public dynamic Pop()
+        {
+            dynamic topValue = this.elements[this.size - 1];
+            this.elements[this.size - 1] = null;
+            this.size -= 1;
+
+            if (this.size <= this.ubound / 2)
+            {
+                this.ubound /= 2;
+            }
+
+            return topValue;
+        }
+
+        public void Push(dynamic valueToPush)
+        {
+            if (this.size + 1 > this.ubound)
+            {
+                if (this.ubound == 0)
+                {
+                    this.ubound = 1;
+                }
+                else
+                {
+                    this.ubound *= 2;
+                }
+
+                Array.Resize<dynamic>(ref this.elements, this.ubound);
+            }
+
+            elements[this.size] = valueToPush;
+            this.size += 1;
+        }
+
+        public dynamic Top()
+        {
+            return this.elements[this.size - 1];
+        }
+    }
+}
---
+++ DylPCustomDS/DylPCustomDSTests/DynamicStackTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DynamicStack = DylPCustomDS.DynamicStack;
+using System;
+
+namespace DylPCustomDSTests
+{
+    [TestClass]
+    public class DynamicStackTests
+    {
+        [TestMethod]
+        public void NewStackWithArrayParameterPopsReverseElements()
+        {
+            dynamic[] initializerArray = new dynamic[] { 1, "2", 3, 4, 5 };
+            DynamicStack newStack = new DynamicStack(initializerArray);
+
+            Assert.AreEqual(5, newStack.size);
+
+            Assert.AreEqual(5, newStack.Pop());
+            Assert.AreEqual(4, newStack.size);
+
+            Assert.AreEqual(4, newStack.Pop());
+            Assert.AreEqual(3, newStack.size);
+
+            Assert.AreEqual(3, newStack.Pop());
+            Assert.AreEqual(2, newStack.size);
+
+            Assert.AreEqual("2", newStack.Pop());
+            Assert.AreEqual(1, newStack.size);
+
+            Assert.AreEqual(1, newStack.Pop());
+            Assert.AreEqual(0, newStack.size);
+        }
+
+        [TestMethod]
+        public void NewStackWithNoParametersIsEmpty()
+        {
+            DynamicStack newStack = new DynamicStack();
+            dynamic[] emptyDynamicArray = new dynamic[0];
+
+            Assert.AreEqual(0, newStack.size);
+            CollectionAssert.AreEqual(emptyDynamicArray, newStack.elements);
+        }
+
+        [TestMethod]
+        public void TopReturnsTopElement()
+        {
+            dynamic[] initializerArray = new dynamic[] { 1, "2", 3, 4, 5 };
+            DynamicStack newStack = new DynamicStack(initializerArray);
+
+            Assert.AreEqual(5, newStack.Top());
+            Assert.AreEqual(5, newStack.size);
+        }
+    }
+}
